10349a24529ff256cf91605e085f3930f1e84d09
Jaume Plomer <jaumeplomer@paucasesnovescifp.cat>
2021-02-25 02:44:02 +0100

    MEs metodes, mañana mas

--- app/src/main/java/com/example/activitat_final_jp/missatges/MissatgeAdapter.java
+++ app/src/main/java/com/example/activitat_final_jp/missatges/MissatgeAdapter.java
@@ -1,4 +1,39 @@
 package com.example.activitat_final_jp.missatges;
 
-public class MissatgeAdapter {
+import android.content.Context;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class MissatgeAdapter extends RecyclerView.Adapter {
+
+    public MissatgeAdapter(Context ct, ArrayList<Missatge> missatges, String idUser)
+    {
+
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+
+        return super.getItemViewType(position);
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
 }
--- app/src/main/java/com/example/activitat_final_jp/xarxa/Auxiliar.java
+++ app/src/main/java/com/example/activitat_final_jp/xarxa/Auxiliar.java
@@ -1,16 +1,28 @@
 package com.example.activitat_final_jp.xarxa;
 
 import android.content.Context;
+import android.net.ConnectivityManager;
+import android.widget.CheckBox;
+import android.widget.LinearLayout;
 
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.activitat_final_jp.database.InterficieBBDD;
+import com.example.activitat_final_jp.missatges.Missatge;
+import com.example.activitat_final_jp.missatges.MissatgeAdapter;
 import com.example.activitat_final_jp.preferencies.Preferencies;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
 
 public class Auxiliar {
     static String a = "a";
@@ -58,8 +70,40 @@ public class Auxiliar {
         return a;
     }*/
 
-    public static void processarMissatges(RecyclerView r, Context c, Preferencies p,String s, boolean b)
+    public static void processarMissatges(RecyclerView rv, Context ct, Preferencies pref, String resultat, boolean prova)
     {
+        try {
+            JSONObject json = new JSONObject(resultat);
+            JSONArray jArr = json.getJSONArray("dades");
+            if (prova)
+            {
+                InterficieBBDD.buidaMissatges();
+            }
+            for (int i = 0; i < jArr.length(); i++)
+            {
+                JSONObject jsonObject = jArr.getJSONObject(i);
+                InterficieBBDD.emmagatzemaRebut(jsonObject);
+            }
+            if (prova)
+            {
+                mostrarMissatges(rv, ct, 0, pref.getCodiusuari());
+            } else {
+                mostrarMissatges(rv, ct, pref.getDarrerMissatge(), pref.getCodiusuari());
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
 
+    public static void mostrarMissatges(RecyclerView rv, Context ct, int darrerMissatge, String idUser)
+    {
+        ArrayList<Missatge> missatges = InterficieBBDD.llistaMissatges(darrerMissatge);
+
+        rv.setHasFixedSize(true);
+        rv.setLayoutManager(new LinearLayoutManager(ct, LinearLayoutManager.VERTICAL, false));
+
+        MissatgeAdapter missatgeAdapter = new MissatgeAdapter(ct, missatges, idUser);
+        rv.setAdapter(missatgeAdapter);
+        rv.smoothScrollToPosition(missatges.size());
     }
 }
